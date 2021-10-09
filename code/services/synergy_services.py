1ddca9deede999c2e969c6b83761b6e1a45e71a9
Edson Castañeda <ecastaneda@diram.com>
2021-10-09 17:57:50 -0500

    feature: update services to filter by transport

--- code/services/synergy_services.py
+++ code/services/synergy_services.py
@@ -26,33 +26,36 @@ class Service(SynergyLogisticsFilters):
                 routes_list.append(route)
         return routes_list
     
-    def get_total_elements(self, direction:str or None = None, year:int or None = None) -> int:
+    def get_total_elements(self, direction:str or None = None, year:int or None = None, transport_mode:str or None = None) -> int:
         """ 
-        Cuenta el número de transacciones en una tabla filtrada. La tabla puede filtrarse por
-        dirección o por año.
+        Cuenta el número de transacciones en una tabla filtrada.
+        Se pueden filtrar resultados por dirección, año y/o medio de transporte.
 
         Args:
             direction (str or None, optional): Dirección de transacción. Defaults to None.
             year (int or None, optional): Año de transacciones. Defaults to None.
+            transport_mode (str or None, optional): Tipo de medio de transporte. Defaults to None.
 
         Returns:
             int: Total de casos en tabla filtrada. 
         """
         # Tabla filtrada
-        filtered_table = self.filter_routes_df(direction=direction, start_year=year, end_year=year)
+        filtered_table = self.filter_routes_df(direction=direction, start_year=year,
+                                               end_year=year, transport_mode=transport_mode)
         # Contar filas en la tabla
         elements_count= len(filtered_table)
         return elements_count
 
-    def get_route_frecuency(self, route:str, direction:str or None = None, year:int or None = None)-> int:
+    def get_route_frecuency(self, route:str, direction:str or None = None, year:int or None = None, transport_mode: str or None = None)-> int:
         """
-        Cuenta las veces que una ruta aparece en una tabla filtrada. La tabla puede filtrarse por
-        dirección o por año.
+        Cuenta las veces que una ruta aparece en una tabla filtrada.
+        Se pueden filtrar resultados por dirección, año y/o medio de transporte.
 
         Args:
             route (str): Rutas con formato origen-destino.
             direction (str or None, optional): Dirección de transacción. Defaults to None.
             year (int or None, optional): Año de transacciones. Defaults to None.
+            transport_mode (str or None, optional): Tipo de medio de transporte. Defaults to None.
 
         Returns:
             int: Numero de apariciones de ruta en la tabla filtrada.
@@ -60,43 +63,47 @@ class Service(SynergyLogisticsFilters):
         # Obtener origen y destino para filtros
         origin, destination = route.split("-")
         # Tabla filtrada
-        filtered_table = self.filter_routes_df(origin=origin, destination=destination, direction=direction, start_year=year, end_year=year)
+        filtered_table = self.filter_routes_df(origin=origin, destination=destination, direction=direction,
+                                               start_year=year, end_year=year, transport_mode=transport_mode)
         # Contar filas en la tabla
         route_frecuency = len(filtered_table)
         return route_frecuency
         
-    def get_total_value(self, direction:str or None = None, year:int or None = None) -> int:
+    def get_total_value(self, direction:str or None = None, year:int or None = None, transport_mode: str or None = None) -> int:
         """
         Suma el valor total dentro de una tabla filtrada.
-        Se pueden filtrar resultados por dirección o año.
+        Se pueden filtrar resultados por dirección, año y/o medio de transporte.
 
         Args:
             route (str): Rutas con formato origen-destino.
             direction (str or None, optional): Dirección de transacción. Defaults to None.
             year (int or None, optional): Año de transacciones. Defaults to None.
+            transport_mode (str or None, optional): Tipo de medio de transporte. Defaults to None.
 
         Returns:
-            int: suma de valor de elementos en tabla filtrada
+            int: suma de valor de elementos en tabla filtrada.
         """
-        filtered_table = self.filter_routes_df(direction=direction, start_year=year, end_year=year)
+        filtered_table = self.filter_routes_df(direction=direction, start_year=year, end_year=year, transport_mode=transport_mode)
         total_value = filtered_table["total_value"].sum()
         return total_value
 
-    def get_route_value(self, route:str, direction:str or None = None, year:int or None = None) -> int:
+    def get_route_value(self, route:str, direction:str or None = None, year:int or None = None, transport_mode: str or None = None) -> int:
         """
         Suma el valor total para una ruta especifica dentro de una tabla filtrada.
-        Se pueden filtrar resultados por dirección o año.
+        Se pueden filtrar resultados por dirección, año y/o medio de transporte.
 
         Args:
             route (str): Rutas con formato origen-destino.
             direction (str or None, optional): Dirección de transacción. Defaults to None.
             year (int or None, optional): Año de transacciones. Defaults to None.
+            transport_mode (str or None, optional): Tipo de medio de transporte. Defaults to None.
 
         Returns:
-            int: suma de valor de elementos en tabla filtrada
+            int: suma de valor de elementos en tabla filtrada.
         """
         origin, destination = route.split("-")
-        filtered_table = self.filter_routes_df(origin=origin, destination=destination, direction=direction, start_year=year, end_year=year)
+        filtered_table = self.filter_routes_df(origin=origin, destination=destination, direction=direction,
+                                               start_year=year, end_year=year, transport_mode=transport_mode)
         route_value = filtered_table["total_value"].sum()
         return route_value
 
